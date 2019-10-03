a52f182f3501cc1c65f7d5f74ab85240ede40edd
Jennifer <jennifertran135@gmail.com>
2019-10-03 04:49:30 +0000

    Initial commit

---
+++ lib/luhn.rb
@@ -0,0 +1,27 @@
+module Luhn
+  def self.is_valid?(number)
+    final_nums = []
+    digits = number.to_s.split('').map(&:to_i) 
+    digits.reverse.each_with_index do |n, index|
+      if index % 2 == 0
+        final_nums << n
+      else
+        double_val = n * 2
+        
+        if double_val >= 10
+          double_val -= 9
+        end
+        final_nums << double_val
+      end
+    end
+    
+    #add up all the digits
+    sum = final_nums.inject(:+)
+
+    if sum % 10 == 0
+      return true
+    else
+      return false
+    end
+  end
+end
